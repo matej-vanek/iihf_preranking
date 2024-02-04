990f397609f20f4ecf1b5e933c1a2925ce8f6c9d
Matěj Vaněk <vanekmatej@gmail.com>
2024-02-04 15:04:24 +0100

    add processing of data

---
+++ iihf/data.py
@@ -0,0 +1,79 @@
+from typing import TypeAlias
+
+import pandas as pd
+from objects import Championship, Event, EventType, OlympicGames, Participant, Placement, process_placement_dicts
+
+EventsType: TypeAlias = dict[Event, dict[Participant, Placement]]
+
+EVENT_TYPE_MAPPING = {
+    "DC": EventType.DEVELOPMENT_CUP,
+    "EC": EventType.EUROPEAN_CHAMPIONSHIP,
+    "SOG": EventType.SUMMER_OLYMPIC_GAMES,
+    "TTT": EventType.THAYER_TRUTT_TROPHY,
+    "WC": EventType.WORLD_CHAMPIONSHIP,
+    "WOG": EventType.WINTER_OLYMPIC_GAMES,
+}
+
+
+def load_data(path: str) -> pd.DataFrame:
+    """Load data od participants, events and placements"""
+    raw_data = pd.read_excel(path, sheet_name=None, engine="odf")
+    events = {}
+    for sheet_name, sheet_data in raw_data.items():
+        if sheet_name == "participants":
+            load_participants_sheet(sheet_data)
+        else:
+            events = load_event(events, sheet_name, sheet_data)
+
+    return process_events(events)
+
+
+def load_participants_sheet(sheet_data: pd.DataFrame) -> None:
+    """Load participants from their sheet"""
+    for _, row in sheet_data.iterrows():
+        Participant(row["code"], row["name_en"], row["name_cs"], row["parent"])
+
+
+def load_event(events: EventsType, sheet_name: str, sheet_data: pd.DataFrame) -> EventsType:
+    """Load event and its placements from sheet"""
+    year, event_type_key = tuple(sheet_name.split("_", maxsplit=1))
+    event_type = EVENT_TYPE_MAPPING.get(event_type_key)
+    event = Event(int(year), event_type)
+
+    placement_dict = {}
+    for _, row in sheet_data.iterrows():
+        participant = Participant.get_participant(row["participant"])
+        if participant in placement_dict:
+            raise ValueError(f"duplicate placement for {participant} in {event}")
+        if not pd.isnull(participant.parent):
+            participant = Participant.get_participant(participant.parent)
+        placement_dict[participant] = Placement(row["rank"])
+    events[event] = placement_dict
+    return events
+
+
+def process_events(events: EventsType) -> pd.DataFrame:
+
+    superevent_data = {}
+    for year in {event.year for event in events}:
+        for superevent_type in (OlympicGames, Championship):
+            matching_events = [
+                (event, participant_placements)
+                for event, participant_placements in events.items()
+                if event.year == year and event.type_ in superevent_type.event_types
+            ]
+            matching_events = sorted(matching_events, key=lambda e_pp: e_pp[0].type_.value)
+            if not matching_events:
+                continue
+            superevent = superevent_type(year)
+            final_placement_dict = process_placement_dicts(
+                [placement_dict for _event, placement_dict in matching_events]
+            )
+
+            superevent_data[superevent] = pd.Series(final_placement_dict.values(), index=final_placement_dict.keys())
+
+    return pd.concat(superevent_data, axis=1)
+
+
+data = load_data("iihf/data.ods")
+print(data.to_string())
--- iihf/objects.py
+++ iihf/objects.py
@@ -2,86 +2,139 @@
 
 from dataclasses import dataclass
 from enum import Enum
+from functools import total_ordering
 from typing import Callable, Optional
 
 
 class EventType(Enum):
     """Type of event"""
 
-    OLYMPIC_GAMES = 1
-    WORLD_CHAMPIONSHIP = 2
-    EUROPEAN_CHAMPIONSHIP = 3
-    THAYER_TRUTT_TROPHY = 4
-    DEVELOPMENT_CUP = 5
+    WINTER_OLYMPIC_GAMES = 1
+    SUMMER_OLYMPIC_GAMES = 2
+    WORLD_CHAMPIONSHIP = 3
+    EUROPEAN_CHAMPIONSHIP = 4
+    THAYER_TRUTT_TROPHY = 5
+    DEVELOPMENT_CUP = 6
 
 
-class SuperEventType(Enum):
-    """Type of superevent"""
-
-    OLYMPICS = 1
-    CHAMPIONSHIP = 2
-
-
-@dataclass
+@total_ordering
 class Participant:
     """Participant of a (super)event, usually a country"""
 
-    code: str
-    name_en: str
-    name_cs: str
-    parent: str
+    all_participants = {}
+
+    def __init__(self, code: str, name_en: str, name_cs: str, parent: str):
+        self._code = code
+        self._name_en = name_en
+        self._name_cs = name_cs
+        self._parent = parent
+        Participant.all_participants[self._code] = self
+
+    def __lt__(self, other):
+        return self.code < other.code
+
+    def __eq__(self, other):
+        return self.code == other.code
+
+    def __hash__(self):
+        return self._code.__hash__()
+
+    def __repr__(self):
+        return f"Participant(code={self.code})"
+
+    @property
+    def code(self):
+        return self._code
+
+    @property
+    def name_en(self):
+        return self._name_en
+
+    @property
+    def name_cs(self):
+        return self._name_cs
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @classmethod
+    def get_participant(cls, code: str) -> Optional["Participant"]:
+        return cls.all_participants.get(code)
 
 
 @dataclass
 class Placement:
     """Placement of a participant within a (super)event"""
 
-    participant: Participant
     rank: int  # starting from 1
     points: Optional[int] = None
 
 
-@dataclass
+@dataclass(frozen=True)
 class Event:
-    """Ice hockey tournament"""
+    """Held ice hockey tournament"""
 
     year: int
     type_: EventType
 
 
-@dataclass
+@dataclass(frozen=True)
 class SuperEvent:
     """Group of events with a strict ordering"""
 
     year: int
-    typ: SuperEventType
+    event_types: list[EventType]
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(year={self.year})"
 
 
-def _process_placement_sets(
-    self, placement_sets: list[set[Placement]]
-) -> set[Placement]:
+class OlympicGames(SuperEvent):
+    event_types = (
+        EventType.WINTER_OLYMPIC_GAMES,
+        EventType.SUMMER_OLYMPIC_GAMES,
+        EventType.THAYER_TRUTT_TROPHY,
+    )
+
+    def __init__(self, year):
+        super().__init__(year, self.event_types)
+
+
+class Championship(SuperEvent):
+    event_types = (
+        EventType.WORLD_CHAMPIONSHIP,
+        EventType.EUROPEAN_CHAMPIONSHIP,
+        EventType.DEVELOPMENT_CUP,
+    )
+
+    def __init__(self, year):
+        super().__init__(year, self.event_types)
+
+
+def process_placement_dicts(placement_dicts: list[dict[Participant, Placement]]) -> dict[Participant, Placement]:
     """Join placement sets into a single set, assign points to placements based on their rank"""
     previous_max_rank = 0
-    max_rank = 0
-    placements = set()
-    for placement_set in placement_sets:
+    final_placements = {}
+    for placement_dict in placement_dicts:
         # get last participant's rank in case ranks could not be shared
         # count of participants cannot be used due to non-systematic ranks
         # of suspended Russia and Belarus
-        ranks = [placement.rank for placement in placement_set]
+        ranks = [placement.rank for placement in placement_dict.values()]
         max_rank = max(ranks)
         max_rank_cardinality = ranks.count(max_rank)
         max_rank += max_rank_cardinality
 
-        for placement in placement_set:
+        for participant, placement in placement_dict.items():
             placement.rank += previous_max_rank
-            placements.add(placement)
-        previous_max_rank = max_rank
+            final_placements[participant] = placement
 
-    formula = self.get_formula(max_rank)
-    for placement in placements:
+        previous_max_rank += max_rank
+
+    formula = get_formula(previous_max_rank)
+    for placement in final_placements.values():
         placement.points = formula(placement.rank)
-    return placements
+    return final_placements
 
 
 def get_formula(max_rank: int) -> Callable[[int], int]:
@@ -90,10 +143,10 @@ def get_formula(max_rank: int) -> Callable[[int], int]:
     points_list = []
 
     points = 20 * max_rank + sum(max_rank >= pb + 1 for pb in point_breaks) * 20
-    for (i,) in range(max_rank):
+    for i in range(max_rank):
         points_list.append(points)
         points -= 20
-        if i - 1 in point_breaks:
+        if i + 1 in point_breaks:
             points -= 20
 
     def _rank_to_points(rank):
